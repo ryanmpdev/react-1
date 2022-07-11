d59db25fb14cc48a57e6626cf5d8cfee8a432911
ryanmpdev <ryanmaciel697@gmail.com>
2022-07-11 10:23:51 -0300

    1 desafio de react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+        state: [
+            {
+                nome: 'Ryan',
+                idade: 16,
+                comidaFavorita: 'carne de panela com batata',
+                music: ["Pais e filhos", "Mandona","paradise" ]
+            }
+        ]
+    }
+    render(){
+        return(
+            <section>
+                <h1>Meu nome é: {this.state.nome}</h1>
+
+                <h2>Minha idade é: {this.state.idade}</h2>
+
+                <h3>Minha comida favorita é: {this.state.comidaFavorita}</h3>
+
+                <ul>
+                    <li>Música 1: {this.state.music[0]}</li>
+                    <li>Música 2: {this.state.music[1]}</li>
+                    <li>Música 3: {this.state.music[2]}</li>
+                </ul>
+            </section>
+        )
+    }
 }
 
 export default App;
