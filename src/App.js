b44c137273eef6f2d59fce3b748817fc81564dbc
ryanmpdev <ryanmaciel697@gmail.com>
2022-07-12 17:13:56 -0300

    Atualização

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
 import React, { Component } from 'react';
-import "./App.css";
 
 class App extends Component {
 
     state = {
-        state: [
-            {
+
                 nome: 'Ryan',
                 idade: 16,
                 comidaFavorita: 'carne de panela com batata',
                 music: ["Pais e filhos", "Mandona","paradise" ]
             }
-        ]
-    }
-    render(){
+   
+    render() {
         return(
-            <section>
+            <div>
                 <h1>Meu nome é: {this.state.nome}</h1>
 
                 <h2>Minha idade é: {this.state.idade}</h2>
 
                 <h3>Minha comida favorita é: {this.state.comidaFavorita}</h3>
 
-                <ul>
-                    <li>Música 1: {this.state.music[0]}</li>
-                    <li>Música 2: {this.state.music[1]}</li>
-                    <li>Música 3: {this.state.music[2]}</li>
-                </ul>
-            </section>
+                <ol>
+                    <li> {this.state.music[0]}</li>
+                    <li> {this.state.music[1]}</li>
+                    <li> {this.state.music[2]}</li>
+                </ol>
+            </div>
         )
+        }
     }
-}
-
 export default App;
